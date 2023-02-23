c6109e3d7c5a431f316ee5e7eef1711a0148ca5c
kakembohenri <mobd3ep@gmail.com>
2023-02-23 09:29:59 +0300

    add: allowed origins in cors policy

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://elivecafe.com/meeting', 'https://elivecafe.com/meeting'],
 
     'allowed_origins_patterns' => [],
 
